404d8a10c4f3310d826b367e6c180d62752dada4
Jcarloluna <91709785+Jcarloluna@users.noreply.github.com>
2022-11-12 20:26:21 +0800

    show password, name input breakpoints

--- ppms_project/profilingApp/static/Javascript/functions.js
+++ ppms_project/profilingApp/static/Javascript/functions.js
@@ -1,8 +1,27 @@
-function myFunction() {
-    var x = document.getElementById("password");
+function showPassword() {
+    var x = document.getElementById("password1");
     if (x.type === "password") {
       x.type = "text";
     } else {
       x.type = "password";
     }
   }
+
+function showPassword2() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function showPassword3() {
+  var x = document.getElementById("cpassword");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
