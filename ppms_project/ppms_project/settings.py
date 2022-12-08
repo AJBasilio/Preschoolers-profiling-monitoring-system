953ee56cb9c36928632a69ab0febdfde8fc31d2f
Jcarloluna <91709785+Jcarloluna@users.noreply.github.com>
2022-12-08 10:18:59 +0800

    mobile nav

--- ppms_project/ppms_project/settings.py
+++ ppms_project/ppms_project/settings.py
@@ -77,26 +77,26 @@ WSGI_APPLICATION = 'ppms_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-  'default': {
-  'ENGINE': 'django.db.backends.mysql',
-  'NAME': 'dasmacit_ppms',
-  'USER': 'dasmacit_ppms',
-  'PASSWORD': "Bet.coets4a",
-  'HOST': "localhost",
-  'PORT': "3306",
-  'OPTIONS': {
-      'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-      }
-   }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# DATABASES = {
+#   'default': {
+#   'ENGINE': 'django.db.backends.mysql',
+#   'NAME': 'dasmacit_ppms',
+#   'USER': 'dasmacit_ppms',
+#   'PASSWORD': "Bet.coets4a",
+#   'HOST': "localhost",
+#   'PORT': "3306",
+#   'OPTIONS': {
+#       'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#       }
+#    }
+#  }
 
 
 # Password validation
